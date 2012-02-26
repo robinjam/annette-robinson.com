0118990ce6a829ba7c6d04eca7a620638fa5af20
James Robinson <admin@robinjam.net>
2012-02-26 10:26:21 +0000

    Update S3 attachment path

--- app/models/image.rb
+++ app/models/image.rb
@@ -13,7 +13,8 @@ class Image < ActiveRecord::Base
       :s3_credentials => {
         :access_key_id => ENV['S3_ACCESS_KEY_ID'],
         :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
-      }
+      },
+      :path => ':attachment/:id/:style'
     }
   else
     STORAGE_OPTIONS = {
