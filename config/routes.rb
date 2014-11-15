3326d6269f2c3c61553d50df8581102f43f325af
James Robinson <admin@robinjam.net>
2014-11-15 00:11:04 +0000

    Fix auth callback in development mode

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     resources :images, only: [:new, :create, :destroy], concerns: [:moveable], controller: 'album_images'
   end
 
-  get '/auth/:provider/callback' => 'sessions#create'
+  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
   get 'logout' => 'sessions#destroy'
 
   root to: 'albums#first'
