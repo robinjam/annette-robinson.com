777af95acd8e6fe37ec32b1d173ebc11c283f4cb
James Robinson <admin@robinjam.net>
2014-11-26 18:22:35 +0000

    Extract S3 bucket to env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    bucket: 'annette-robinson',
+    bucket: ENV['S3_BUCKET'],
     s3_credentials: {
       access_key_id: ENV['S3_ACCESS_KEY_ID'],
       secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
